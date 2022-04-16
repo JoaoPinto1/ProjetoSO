d0ab5148e65b67270f381b0d316f6d7d2cff847e
ThankTiago <102809390+ThankTiago@users.noreply.github.com>
2022-04-16 22:52:34 +0100

    Update log.c
    struct tm é um pointer hate myself

--- log.c
+++ log.c
@@ -2,7 +2,7 @@
 
 void getcurrtime(char *curr) {
     time_t t = time(NULL);
-    struct tm info = localtime(&t);
+    struct tm *info = localtime(&t);
     strftime(curr, TIMELEN, "%H:%M:%S", &info);
 }
 
