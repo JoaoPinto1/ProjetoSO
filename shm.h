d9e63f51afa5d5b3ec64c81eaba8a7ba11d6767d
ThankTiago <102809390+ThankTiago@users.noreply.github.com>
2022-05-01 23:04:29 +0100

    Add files via upload

---
+++ shm.h
@@ -0,0 +1,44 @@
+#ifndef SHM_H
+#define SHM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#define SHM_NAME "SHM"
+
+typedef struct {
+    int queuePos;
+    int maxWait;
+    int num_servers;
+    FILE *log_file;
+} configs;
+
+typedef struct {
+    int speed;
+    float wait_time;
+    int nivel;
+} vcpu;
+
+typedef struct{
+    char name[32];
+    vcpu vcpus[2];
+} edgeServer;
+
+extern void *shm_pointer;
+extern configs *conf;
+extern edgeServer *servers;
+extern pthread_mutex_t *log_mutex;
+
+
+#endif //TASK_MANAGER_H
