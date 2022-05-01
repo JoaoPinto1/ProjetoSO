198afd1350760b4c5a00d9e2408d5c164d3fa7e4
ThankTiago <102809390+ThankTiago@users.noreply.github.com>
2022-05-01 23:08:47 +0100

    Add files via upload

--- log.h
+++ log.h
@@ -7,12 +7,14 @@
 #ifndef LOG_H
 #define LOG_H
 
-#define LOGFILE "log.txt"
-#define TIMELEN 9
 #include <stdio.h>
 #include <time.h>
+#include "shm.h"
+#define LOGFILE "log.txt"
+#define TIMELEN 9
 
 void getcurrtime(char *);
 void logfunc(char *,FILE *);
+void sync_log(char *s, FILE *f);
 
 #endif //LOG_H
